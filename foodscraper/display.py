89ae05b649593d8d49f7c6e5808aaed8f0b3fcc1
ryan570 <brownryan570@gmail.com>
2019-09-27 12:11:00 -0400

    display protein data

--- foodscraper/display.py
+++ foodscraper/display.py
@@ -1,14 +1,16 @@
 from flask import Blueprint, render_template
 
 from item import Item
-#from scraper import check_menu, get_protein
+from scraper import check_menu, get_protein
 from cache import write_data, read_data
 
 display = Blueprint('display', __name__, template_folder='templates')
 
-#chase = get_protein("chase")
-#write_data(chase, 'menu.json')
-chase = read_data('menu.json')
+chase = get_protein("chase")
+write_data(chase, 'chase.json')
+lenoir = get_protein("lenoir")
+write_data(lenoir, 'lenoir.json')
+#chase = read_data('chase.json')
 
 @display.route('/')
 def home():
@@ -18,10 +20,18 @@ def home():
 def hall(hall):
     data = chase
 
-    food = []
+    menu = {}
+    timeslots = []
 
     for timeslot in data:
+        timeslots.append(timeslot)
+        food = []
         for item in data[timeslot]:
             food.append(Item(item, data[timeslot][item]))
+        menu[timeslot] = food
 
-    return render_template('display.html', food=food)
+    first = timeslots.pop(0)
+    timeslots.reverse()
+    timeslots.insert(0, first)
+
+    return render_template('display.html', food=menu, timeslots=timeslots)
